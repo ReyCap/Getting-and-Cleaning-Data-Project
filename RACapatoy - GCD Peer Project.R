8e5f4bc1849513790aa852dca0b05d082de6853e
ReyCap <51155928+ReyCap@users.noreply.github.com>
2019-08-01 15:35:56 +0200

    Add files via upload

---
+++ RACapatoy - GCD Peer Project.R
@@ -0,0 +1,63 @@
+#Peer Project
+
+#####Loading the library
+library(dplyr)
+
+#Extracting the data from the zip file
+filename <- "getdata_projectfiles_UCI HAR Dataset.zip"
+
+## Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+
+###Assigning all data frames
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
+
+
+#####Running the Analysis
+
+#Step1 - Merge the training and test data sets to create a merge data
+X <- rbind(x_train, x_test)
+Y <- rbind(y_train, y_test)
+Subject <- rbind(subject_train, subject_test)
+Merged_Data <- cbind(Subject, Y, X)
+
+#Step2 - Extracts only the measurements on the mean and stddev for each measurement
+TidyData <- Merged_Data %>% 
+  select(subject, code, contains("mean"), contains("std"))
+
+#Step3 - Uses activity name descriptions to name the activities
+TidyData$code <- activities[TidyData$code, 2]
+
+#Step4 - Labels the data set with descriptive variable names
+names(TidyData)[2] = "activity"
+names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
+names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
+names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
+names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
+names(TidyData)<-gsub("^t", "Time", names(TidyData))
+names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
+names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
+names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("angle", "Angle", names(TidyData))
+names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
+
+#Step5 - From data in Step4, created a 2nd data that contains the average of each variable
+FinalData <- TidyData %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(FinalData, "FinalData.txt", row.name=FALSE)
+
+#Last check to scrutinize the variable names
+str(FinalData)
